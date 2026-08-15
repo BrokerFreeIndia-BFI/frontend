e152d4a4b9995decd939bb79b22720023cd0483e
sayanrex007 <manna.sayan@icloud.com>
2026-08-15 21:40:52 +0530

    SCRUM-31: navbar added (#2)
    * navbar added
    
    * bugfix/SCRUM-32: Linting fixed

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,9 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/home';
+import { Navbar } from './components/navbar';
 
 export function App() {
   return (
     <BrowserRouter>
+      <div className="relative w-full flex items-center justify-center">
+        <Navbar className="top-2" />
+      </div>
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
